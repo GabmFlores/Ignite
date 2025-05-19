c81f5d641de9af4b51a2d9441e0fdae058073569
Gabriel Mari Flores <gmflores@gbox.adnu.edu.ph>
2025-05-20 02:36:32 +0800

    first real commit

--- ignite/src/App.tsx
+++ ignite/src/App.tsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import NavHome from "./components/NavHome";
+import Home from "./pages/home";
+import FAQ from "./pages/Faq";
+import About from "./pages/about";
 
 function App() {
   return (
-    <main>
-      <NavHome></NavHome>
-      <h1 className="text-primary">Ignite</h1>
-    </main>
+    <>
+      <NavHome />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
--- ignite/src/components/NavHome.tsx
+++ ignite/src/components/NavHome.tsx
@@ -1,17 +1,34 @@
+import { NavLink, useLocation } from "react-router-dom";
 import Logo from "../assets/ignitelogo.png";
 
 const NavHome = () => {
+  const location = useLocation();
+
+  const linkClass = (path: string) =>
+    location.pathname === path
+      ? "text-primary2 font-bold underline underline-offset-8 decoration-2"
+      : "text-primary2";
+
   return (
-    <div className="flex justify-between bg-white rounded-3xl p-4 m-8">
-      <a href="/">
-        <img src={Logo} alt="Logo" className="h-16" />
-      </a>
-      <div className="flex items-center gap-2 justify-evenly">
-        <div>Home</div>
-        <div>FAQ</div>
-        <div>ABOUT</div>
-        <div>Register</div>
-        <div>Sign In</div>
+    <div className="mx-8">
+      <div className="flex shadow-xl justify-between bg-white rounded-3xl p-4 my-8 max-w-4xl mx-auto">
+        <a href="/">
+          <img src={Logo} alt="Logo" className="h-[clamp(3rem,5vw,4rem)]" />
+        </a>
+        <div className="flex items-center gap-8 justify-evenly text-[clamp(1.2rem,2vw,1.5rem)] mx-2">
+          <NavLink to="/" className={linkClass("/")}>
+            Home
+          </NavLink>
+          <NavLink to="/faq" className={linkClass("/faq")}>
+            FAQ
+          </NavLink>
+          <NavLink to="/about" className={linkClass("/about")}>
+            ABOUT
+          </NavLink>
+        </div>
+        <div className="flex items-center gap-4 justify-evenly text-[clamp(1.2rem,2vw,1.5rem)] mx-2">
+          <div>Sign In</div>
+        </div>
       </div>
     </div>
   );
